6a87095400de6681ddafc907b0f51cb751981603
SalwaRaa <saalwa93@live.se>
2020-12-03 12:48:16 +0100

    Updated  the test methods inserted the bowling tesd methods

--- BowlingClassLibrary.UnitTests/RoundTests.cs
+++ BowlingClassLibrary.UnitTests/RoundTests.cs
@@ -25,7 +25,7 @@ namespace BowlingClassLibrary.UnitTests
                 r.Roll(pins);
             }
         }
-       
+
         [Fact]
         public void Should_Roll_Gutter()
         {
@@ -39,8 +39,38 @@ namespace BowlingClassLibrary.UnitTests
             should_Roll_Many(20, 1);
             Assert.Equal(20, r.Score());
         }
-        
-      
+
+        private void Should_Roll_Spare()
+        {
+            r.Roll(6);
+            r.Roll(4);
+        }
+
+        [Fact]
+        public void Should_Roll_OneSpare()
+        {
+            Should_Roll_Spare();
+            r.Roll(4);
+            should_Roll_Many(17, 0);
+            Assert.Equal(18, r.Score());
+        }
+
+        [Fact]
+        public void Should_Roll_OneStrike()
+        {
+            r.Roll(10);
+            r.Roll(4);
+            r.Roll(5);
+            should_Roll_Many(17, 0);
+            Assert.Equal(28, r.Score());
+        }
+
+        [Fact]
+        public void Should_Roll_Perfect_Game()
+        {
+            should_Roll_Many(12, 10);
+            Assert.Equal(300, r.Score());
+        }
 
     }
 }
--- BowlingClassLibrary/Round.cs
+++ BowlingClassLibrary/Round.cs
@@ -2,37 +2,58 @@
 {
     public class Round
     {
-            private int[] rolls = new int[21];
-            private int[] frame = new int[10];
-            int currentRoll = 0;
-            
-            public void Roll(int pins)
+        private int[] rolls = new int[21];
+        private int[] frame = new int[10];
+        int currentRoll = 0;
+
+        private bool IsSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll++] = pins;
+        }
+
+        public void Roll(int[] pins)
+        {
+            for (int i = 0; i < pins.Length; i++)
             {
-                rolls[currentRoll++] = pins;
-            }
-
-            public void Roll(int[] pins)
-            {
-                for (int i = 0; i < pins.Length; i++)
-                {
-                    rolls[currentRoll++] = pins[i];
-                }
-            }
-
-            public int Score()
-            {
-                int score = 0;
-                int frameIndex = 0;
-                for (int frame = 0; frame < 10; frame++)
-                {
-                    {
-                        score += rolls[frameIndex] + rolls[frameIndex + 1];
-                        frameIndex += 2;
-                    }
-
-                }
-
-                return score;
+                rolls[currentRoll++] = pins[i];
             }
         }
+
+        public int Score()
+        {
+            int score = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else if (IsStrike(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+
+            }
+
+            return score;
+        }
+    }
 }
